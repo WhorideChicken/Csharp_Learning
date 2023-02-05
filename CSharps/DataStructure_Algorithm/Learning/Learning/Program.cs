9ec76c613a136e81b3506b6ba26b09996224a0e2
Jin <wlsdndnjs@gmail.com>
2023-02-06 01:09:02 +0900

    feat : 길찾기 알고리즘 프로젝트를 위한 셋팅

---
+++ CSharps/DataStructure_Algorithm/Learning/Learning/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i =0; i < 25; i ++)
+            {
+                for(int j = 0; j < 25; j++)
+                    Console.Write('\u25cf');
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
