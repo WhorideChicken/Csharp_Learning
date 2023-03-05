03dd2e1c6444c92bfbb2373f43a1923cff11b2e0
Jin <wlsdndnjs@gmail.com>
2023-03-05 22:59:45 +0900

    feat : C# 미로그리기 초기 로직
    - Frame 계산 로직 추가
    - 외곽선 및 미로 판 그리는 로직 추가

--- CSharps/DataStructure_Algorithm/Learning/Learning/Board.cs
+++ CSharps/DataStructure_Algorithm/Learning/Learning/Board.cs
@@ -6,11 +6,72 @@ using System.Threading.Tasks;
 
 namespace Learning
 {
+    //배열을 이용해서 미로를 만들자
+
     class Board
     {
-        public static void Main(string[] args)
+        public TileType[,] _tile;
+        public int _size;
+        const char CIRCLE = '\u25cf';
+
+        public enum TileType
         {
-            //a
+            Empty,
+            Wall
         }
+
+
+        public void Initialize(int size)
+        {
+            _tile = new TileType[size, size];
+            _size = size;
+
+           
+            //갈 수 있는 지역, 갈 수 없는 지역을 나누자
+            for(int y = 0; y < _size; y++)
+            {
+                for(int x = 0; x < _size; x++)
+                {
+                    if (x == 0 || x == size - 1 || y == 0 || y == size - 1)
+                        _tile[y, x] = TileType.Wall;
+                    else
+                        _tile[y, x] = TileType.Empty;
+                }
+            }
+
+        }
+
+        public void Renderer()
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    Console.ForegroundColor = GetTileColor(_tile[y, x]);
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = prevColor;
+        }
+
+
+        ConsoleColor GetTileColor(TileType type)
+        {
+            switch (type)
+            {
+                case TileType.Empty:
+                    return ConsoleColor.Green;
+                case TileType.Wall:
+                    return ConsoleColor.Red;
+                default:
+                    return ConsoleColor.Green;
+            }
+
+        }
+
     }
 }
--- CSharps/DataStructure_Algorithm/Learning/Learning/MainPrograms.cs
+++ CSharps/DataStructure_Algorithm/Learning/Learning/MainPrograms.cs
@@ -8,5 +8,50 @@ namespace Learning
 {
     class MainPrograms
     {
+
+
+        public static void Main(string[] args)
+        {
+            Board board = new Board();
+            board.Initialize(25);
+
+            //커서가 안보이도록 환경 설정
+            Console.CursorVisible = false;
+
+            //프레임 관리
+            //FPS 프레임(60프레임 정도면 부드럽고 30프레임 이하면 게임 하기 어렵다)
+            //1초에 아래 입력, 로직, 랜더링 로직이 몇번 진행되는지가 프렘임이다
+            //프레임 계산
+
+            const int WAIT_TICK = 1000 / 30;
+            //마지막으로 측정한 시간
+            float lastTick = 0;
+
+            while (true)
+            {
+                #region Frame
+                //프레임 : 현재 시간
+                float currentTick = System.Environment.TickCount;//밀리 세컨드이다 1초 == 1000ms
+                                                                 //경과 된 시간 현재시간 - 마지막 시간
+                                                                 //만약에 경과한 시간이 1/30초보다 작다면 continue 밀리 세컨드이다 1초 == 1000ms
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue; //30프레임이 안나오면 아래 로직을 돌리지 않고 다시 while문 초기로 
+
+                //1/30보다 경과 됐다면 시간을 기록하여 다음 루프 때 다시 측정한다. 
+                lastTick = currentTick;
+
+                #endregion Frame
+
+                //입력 : 키보드 마우스 인풋
+
+                //로직 : 입력에 따른 동작된 게임 로직
+
+                //랜더링 : 게임을 그려주는 랜더링
+
+                //콘솔 커서 위치 고정
+                Console.SetCursorPosition(0, 0);
+                board.Renderer();
+            }
+        }
     }
 }
